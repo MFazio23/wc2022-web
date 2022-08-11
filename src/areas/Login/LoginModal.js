f9a54e995b01dbd0201d5480734038b86bec753d
Scott Johnson <singinbaritone@gmail.com>
2022-08-11 00:39:09 -0400

    initial commit for login

---
+++ src/areas/Login/LoginModal.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import {Backdrop, Box, Modal, Fade, Button} from '@mui/material';
+import StyledOAuthLogin from './Login.js'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function LoginModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Login</Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <StyledOAuthLogin/>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  )
+}
