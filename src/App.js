d1f25d2746ff290762a420fe10cad37d4bd657fd
Michael Fazio <mfazio23@gmail.com>
2022-09-04 15:27:43 -0500

    Missed the App updates

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 import LoginModal from './areas/Login/LoginModal';
 import {auth} from './areas/Login/FirebaseConfig';
 
 
 function App() {
-  const [isSignedIn, setIsSignedIn] = useState(undefined)
+    const [isSignedIn, setIsSignedIn] = useState(undefined)
 
-  useEffect(()=>{
-    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-      setIsSignedIn(!!user);
-    });
-    return () => unsubscribe();
-  }, []);
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+            setIsSignedIn(!!user);
 
-  if (!isSignedIn){
+            user.getIdToken().then(result => {
+                console.log("User ID token", result);
+            })
+        });
+        return () => unsubscribe();
+    }, []);
+
+    if (!isSignedIn) {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <LoginModal isSignedIn={isSignedIn}/>
+                </header>
+            </div>
+        );
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <LoginModal isSignedIn={isSignedIn} />
-        </header>
-      </div>
-    );
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome {auth.currentUser.displayName}! You are now signed in.</p>
-        <a onClick={()=> auth.signOut()}>Sign Out</a>
-      </header>
-    </div>
-  )
+        <div className="App">
+            <header className="App-header">
+                <p>Welcome {auth.currentUser.displayName}! You are now signed in.</p>
+                <a onClick={() => auth.signOut()}>Sign Out</a>
+            </header>
+        </div>
+    )
 
 }
 
